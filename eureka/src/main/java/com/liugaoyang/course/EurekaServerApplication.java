667454efc032d80e254237e0b0885ee12404e79b
lgy <31435840+superliuliuliu@users.noreply.github.com>
2020-07-12 16:02:53 +0800

    1.引入Eureka server注册子模块服务

--- eureka/src/main/java/com/liugaoyang/course/EurekaServerApplication.java
+++ eureka/src/main/java/com/liugaoyang/course/EurekaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
