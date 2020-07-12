014b67afbec169f153ed6743b1b8ac4999d55a05
lgy <31435840+superliuliuliu@users.noreply.github.com>
2020-07-12 16:48:09 +0800

    1.新增System子模块

--- eureka/src/main/java/com/liugaoyang/course/EurekaServerApplication.java
+++ eureka/src/main/java/com/liugaoyang/course/EurekaServerApplication.java
@@ -1,13 +1,22 @@
 package com.liugaoyang.course;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
+
+@EnableEurekaServer /*服务注册中心*/
 @SpringBootApplication
+@Slf4j
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！");
+		log.info("Eureka Server地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
